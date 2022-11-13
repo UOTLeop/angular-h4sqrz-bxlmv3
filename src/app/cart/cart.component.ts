e837952599d39d30cf39a7e1879c7f62a53c232c
UOTLeop <jialiang.lin@mail.utoronto.ca>
2022-11-13 11:23:59 -0800

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-
-
+  items = this.cartService.getItems();
+  constructor(
+    private cartService: CartService
+  ) { }
 }
